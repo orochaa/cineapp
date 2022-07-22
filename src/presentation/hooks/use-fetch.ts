c9cfd2c1288a0773369e3486b5a36f72c8c953c0
Bruno Rocha <brunorocha2674@gmail.com>
2022-07-22 00:48:46 -0300

    fix: add vite api url to axios client

--- src/presentation/hooks/use-fetch.ts
+++ src/presentation/hooks/use-fetch.ts
@@ -5,11 +5,11 @@ export function useFetch<T = undefined> (uri: string) {
   const { data, error, mutate } = useSWR<T>(uri, async uri => {
     const res = await api.get(uri, {
       params: {
-        api_key: import.meta.env.API_TOKEN,
+        api_key: import.meta.env.VITE_API_TOKEN,
         language: 'pt-BR'
       }
     })
-    return res.data
+    return res.data.results
   })
   return { data, error, mutate }
 }
