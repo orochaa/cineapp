f2fdaaa85947f650bd7978a4b11282f475ddcd53
Bruno Rocha <brunorocha2674@gmail.com>
2022-07-22 18:38:06 -0300

    chore: add api key enviroment variable

--- src/presentation/hooks/use-fetch.ts
+++ src/presentation/hooks/use-fetch.ts
@@ -1,11 +1,11 @@
 import useSWR from 'swr'
 import { api } from '@/infra/http'
 
-export function useFetch<T = undefined> (uri: string) {
+export function useFetch<T> (uri: string) {
   const { data, error, mutate } = useSWR<T>(uri, async uri => {
     const res = await api.get(uri, {
       params: {
-        api_key: import.meta.env.VITE_API_TOKEN,
+        api_key: import.meta.env.VITE_API_KEY,
         language: 'pt-BR'
       }
     })
