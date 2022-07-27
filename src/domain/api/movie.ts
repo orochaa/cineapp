c5e3bc2f68a686f3c92522ded9af05363fcde734
Bruno Rocha <brunorocha2674@gmail.com>
2022-07-27 18:00:00 -0300

    chore: add more movie interfaces

--- src/domain/api/movie.ts
+++ src/domain/api/movie.ts
@@ -29,3 +29,54 @@ export type IMovieGenre =
   | 'romance'
   | 'sciFi'
   | 'western'
+
+export interface IMovieDetails extends IMovie {
+  budget: number
+  release_date: string
+  revenue: number
+  runtime: number
+  tagline: string
+  genres: Array<{ id: number; name: string }>
+}
+
+export interface IMovieVideo {
+  id: string
+  name: string
+  key: string
+}
+
+export interface IMovieCredits {
+  cast: Array<{
+    id: number
+    known_for_department: string
+    name: string
+    original_name: string
+    popularity: number
+    profile_path: string
+    cast_id: number
+    character: string
+    credit_id: string
+    order: number
+  }>
+  crew: Array<{
+    id: number
+    known_for_department: string
+    name: string
+    original_name: string
+    popularity: number
+    profile_path: string
+    credit_id: string
+    job: string
+    department: string
+  }>
+}
+
+export interface IMovieProviders {
+  BR: {
+    flatrate?: Array<{
+      logo_path: string
+      provider_id: number
+      provider_name: string
+    }>
+  }
+}
